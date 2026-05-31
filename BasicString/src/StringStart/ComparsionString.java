45ad61462c65db48326d94b82aef0f49c37ae006
muskan <muskantuteja714@gmail.com>
2026-05-31 15:01:48 +0530

    DSA

---
+++ BasicString/src/StringStart/ComparsionString.java
@@ -0,0 +1,18 @@
+package StringStart;
+
+public class ComparsionString {
+	public static void main(String[] args) {
+		String name1 = "Muskan";
+		String name2 = "Muskan";
+		
+		
+//		== userd
+		if(name1 == name2) {
+			System.out.println("Both string are Equal");
+		}else {
+			System.out.println("Both are not equal");
+			
+		}
+	}
+
+}
