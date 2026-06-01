ad397af41e6443b80d361444803a987a87d17e37
muskan <muskantuteja714@gmail.com>
2026-06-01 23:12:27 +0530

    reverse

--- BasicString/src/QuestionSloveString/ReverseOfString.java
+++ BasicString/src/QuestionSloveString/ReverseOfString.java
@@ -1,5 +1,16 @@
 package QuestionSloveString;
 
 public class ReverseOfString {
+	
+	public static void main(String[] args) {
+		String str = "Hello";
+		String reverse = new StringBuilder(str).reverse().toString();
+		System.out.println(reverse);
+	}
+	
+	
+
 
 }
+
+
