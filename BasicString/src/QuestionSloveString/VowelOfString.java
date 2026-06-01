a3470931b34a2b1518203723dbcc571efd9e876f
muskan <muskantuteja714@gmail.com>
2026-06-01 23:01:43 +0530

    vowels

--- BasicString/src/QuestionSloveString/VowelOfString.java
+++ BasicString/src/QuestionSloveString/VowelOfString.java
@@ -1,5 +1,23 @@
 package QuestionSloveString;
 
 public class VowelOfString {
+	static int getVowelsCount(String str) {
+		int count = 0;
+		for(int i=0; i<str.length();i++) {
+			char ch = str.charAt(i);
+			if(ch=='a' || ch=='e' || ch=='M' || ch=='u') {
+				count++;
+			}
+			
+			
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		String str = "Muskan";
+		System.out.println(getVowelsCount(str));
+	}
+	
+	
 
 }
